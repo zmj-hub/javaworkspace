26a22af9413b2991a9cb5844332cac16fc817fed
zhangmingjie <zhangmj@jianjukeji.com>
2019-12-06 13:52:23 +0800

    递归 1到100相加

--- src/TestDiGui.java
+++ src/TestDiGui.java
@@ -1,17 +1,16 @@
 public class TestDiGui {
     public static void main(String[] args) {
-        //System.out.println(fun(100));
-//        String 王泽乾="是王八蛋";
-//        System.out.println("王泽乾"+王泽乾);
 
-        String a="weyquryq";
-        System.out.println(a);
+        System.out.println(fun(100));
+
     }
 
-//    public static int  fun(int i) {
-//        if(i==1){
-//            return 1;
-//        }
-//        return fun(i-1)+i;
-//    }
+    public static int fun(int i) {
+        if (i == 1) {
+            return 1;
+        } else {
+            return i + fun(i - 1);
+        }
+    }
+
 }
